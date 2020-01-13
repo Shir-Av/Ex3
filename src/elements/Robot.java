4be60acbac1e1b3bc9e5397f90e3881f862d594d
yuvalzarbiv <yoval1240@gmail.com>
2020-01-13 19:29:21 +0200

    update gui

--- src/elements/Robot.java
+++ src/elements/Robot.java
@@ -3,6 +3,9 @@ package elements;
 import org.json.JSONObject;
 import utils.Point3D;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Robot {
 
     public int id;
@@ -12,13 +15,44 @@ public class Robot {
     public int dest;
     public Point3D location;
     private static int i = 0;
+    private String img = "robot.png";
 
     public Robot()
     {
         this.id  = i++;
         this.value = 0;
         this.speed = 0;
+        this.img = "robot.png";
     }
+    public Robot(Point3D p)
+    {
+        this.location = p;
+        this.id  = i++;
+        this.img = "robot.png";
+    }
+
+    public Robot(String s) {
+        try {
+            JSONObject Robot = new JSONObject(s);
+            JSONObject robot = Robot.getJSONObject("Robot");
+            String pos = robot.getString("pos");
+            this.location = new Point3D(pos);
+            this.value = robot.getInt("value");
+            this.id = robot.getInt("id");
+            this.img = "robot.png";
+            this.src =  robot.getInt("src");
+            this.dest =  robot.getInt("dest");
+            }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+
+
     public int getId() {
         return id;
     }
@@ -65,6 +99,14 @@ public class Robot {
 
     public void setLocation(Point3D location) {
         this.location = location;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
     }
 }
 
